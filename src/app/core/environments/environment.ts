14fe5d53a629cfdf621608d4bbc4c10d83684b82
Marwa Ali Mostafa <marwa.a.mostafa205@gmail.com>
2025-12-18 13:56:30 +0200

    Update: description of changes

--- src/app/core/environments/environment.ts
+++ src/app/core/environments/environment.ts
@@ -18,7 +18,7 @@ export const environment = {
 
 export const environmentProduction = {
   production: true,
-  apiUrl: 'https://mahd2.runasp.net/api', // Update with your production API URL
+  apiUrl: 'http://mahd2.runasp.net/api', // Update with your production API URL
   deviceId: generateDeviceId()
 };
 
